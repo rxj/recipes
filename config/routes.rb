29aad4bf78453858c9e5ef8dd84a277f5bc0c2b1
rxj <roxana.junczyk@gmail.com>
2013-12-14 11:47:22 +0100

    Startseite

--- config/routes.rb
+++ config/routes.rb
@@ -62,7 +62,7 @@ Glossary::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
   # map.root :controller => "glosentries"
-root :to => 'glosentries#index'
+root :to => redirect("/glosentries")
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
